7d458ffec567e9f00cb152d190bad01d9d644fce
dangtruong-github <127490669+dangtruong-github@users.noreply.github.com>
2026-04-26 13:51:05 +0700

    add arm fp32xint8=fp32 path

---
+++ matmul/include/arm_utils.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <arm_neon.h>
+
+static inline float max_reduce_float32x4(float32x4_t reg) {
+    
+}
--- matmul/include/cpu_wrapper.hpp
+++ matmul/include/cpu_wrapper.hpp
@@ -3,7 +3,12 @@
 #include <stdio.h>
 #include <omp.h>
 #include <sched.h> // for sched_setaffinity(), cpu_set_t, CPU_ZERO, CPU_SET
+
 #include "../../utils/module.hpp"
+
+#ifdef __ARM_NEON
+#include "arm_utils.hpp"
+#endif
 
 void linear(
     const void *mat_A, const void *mat_B_in, const void *mat_B_scale,
@@ -37,3 +42,11 @@ void gemm_text_kv_att(
     const size_t max_pos, const size_t group_size, DType::Type type_a,
     DType::Type type_b, DType::Type type_b_s, DType::Type type_c
 );
+
+#ifdef __ARM_NEON
+void arm_f32a_i8f32sb_f32c(
+    const float* mat_A, const int8_t* mat_B_in,
+    const float* mat_B_scales, const int *sum_int8_B, float* mat_C,
+    size_t M, size_t N, size_t K, size_t group_size, bool add_to_c
+);
+#endif
